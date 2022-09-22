8bebb38e145a3a6482b14573ed1f8247511bf397
Nikita <Nikos48lip@gmail.com>
2022-09-22 16:18:47 +0300

    9.1. String [#237407]

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -4,9 +4,7 @@ public class SqArea {
     public static double square(double p, double k) {
         double h = p / (2 * (k + 1));
         double l = h * k;
-        double s = l * h;
-        double rsl = 2 * s;
-        return rsl;
+        return 2 * (l * h);
     }
 
     public static void main(String[] args) {
